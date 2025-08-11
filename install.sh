cb8f781f37ccc658d61f3e163d1fabd5de66a027
Jarred Peterson <spidy_peterson@hotmail.com>
2025-08-11 18:32:45 +1000

    Typo fixed

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Define paths
 INSTALL_DIR="/data"
-REPO_AUTHOR="spidy01"
+REPO_AUTHOR="Spidy01"
 REPO_NAME="dbus-canbus-battery"
 ZIP_NAME="repo.zip"
 TMP_DIR="${INSTALL_DIR}/${REPO_NAME}-main"
